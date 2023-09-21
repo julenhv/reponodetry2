331f971a7670b418e221fbfe9ff8fdd6d70c48be
jhelguera <julen.helguera@thesimonproject.com>
2023-09-21 17:02:11 +0200

    commit 88

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+//Redux
+
+
+import 'materialize-css/dist/css/materialize.min.css' 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider, createSelectorHook } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+
+import App from './components/App';
+import reducers from './reducers'
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+
+ReactDOM.render(
+    <Provider store={store}><App/></Provider>,
+    document.querySelector('#root')
+);
+
+
--- services/passport.js
+++ services/passport.js
@@ -2,24 +2,18 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose')
 const keys = require('../config/keys')
-
 const User = mongoose.model('users');
-
 
 passport.serializeUser((user,done) => {
     done(null,user.id);
 
 });
-
 passport.deserializeUser((id,done) => {
     User.findById(id)
         .then(user => {
             done(null,user);
     })
 });
-
-
-
 passport.use(
     new GoogleStrategy(
         {
@@ -28,20 +22,18 @@ passport.use(
         callbackURL: '/auth/google/callback',
         proxy: true
         }, 
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({googleId: profile.id})
-                .then((existingUser)=> {
-                    if (existingUser){
-                        //we already have a record with the given profile id
-                        done(null, existingUser)
-                    } else{
-                        // we don't have a user record with this ID, make a new record
-                        new User({googleId: profile.id})
-                            .save()
-                            .then(user=> done(null, user));
-                    }
-                    
-                })
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({googleId: profile.id});
+
+            if (existingUser){
+                //we already have a record with the given profile id
+                done(null, existingUser)
+            } else{
+                // we don't have a user record with this ID, make a new record
+                const user = await new User({googleId: profile.id}).save()
+                done(null, user);
+            }
+
         }
     )
 );
