cc4bbb2117a943f109dc1c9eb1bdc006952f7c55
jhelguera <julen.helguera@thesimonproject.com>
2023-09-25 17:43:35 +0200

    commit chapt 94

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, createSelectorHook } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers'
 
-const store = createStore(() => [], {}, applyMiddleware());
+
+
+const store = createStore(() => [], {}, applyMiddleware(reduxThunk));
 
 
 ReactDOM.render(
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,5 @@
-
-
-
-
-
-
 export default function(state, action){
+    console.log(action);
     switch (action.type){
         default:
             return state ;
