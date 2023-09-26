f3c2106c0fc61622752974bd67af668a51e29c1e
jhelguera <julen.helguera@thesimonproject.com>
2023-09-26 17:40:36 +0200

    commit end chapter 8

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const passport = require ('passport');
 const cookieSession = require ('cookie-session');
 const keys = require('./config/keys')
-
+const bodyParser = require ('body-parser')
 
 require('./models/User'); 
 require('./services/passport');
@@ -12,6 +12,10 @@ mongoose.connect(keys.mongoURI);
 
 
 const app = express();
+
+app.use(bodyParser.json())
+
+
 app.use(
     cookieSession({
         maxAge: 30**24*60*60*1000,
@@ -22,7 +26,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 require('./routes/authRoutes')(app);
+require('./routes/billingroutes')(app);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
