0c10e80f00461aadeb8273bb7971ce7991ba3aad
jhelguera <julen.helguera@thesimonproject.com>
2023-09-27 13:57:19 +0200

    commit chapt 143

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const keys = require('./config/keys')
 const bodyParser = require ('body-parser')
 
 require('./models/User'); 
+require('./models/Survey'); 
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -28,13 +29,14 @@ app.use(passport.session());
 
 
 require('./routes/authRoutes')(app);
-require('./routes/billingroutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production'){
     //Express will serve up production assets
     //like our main.js file, or main.css file!
     app.use(express.static('client/build'));
-    
+
     //Epress will serve up the index.html file
     // if it doesn't recognize the route
     const path = require ('path');
