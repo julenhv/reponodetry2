3d57507cd628f29b312c025d3b2f3e07942ff488
jhelguera <julen.helguera@thesimonproject.com>
2023-09-27 10:51:17 +0200

    authreducers typo

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_USER } from "../actions/types";
 
 
-export default function(stat = null, action){
+export default function(state = null, action){
     switch (action.type) {
         case FETCH_USER:
             return action.payload || false;
