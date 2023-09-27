d67648ee6ed0377ad5b0cb72e8dcd9d617d8b123
jhelguera <julen.helguera@thesimonproject.com>
2023-09-27 11:06:08 +0200

    forgotten line in index.js

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ require('./routes/billingroutes')(app);
 if (process.env.NODE_ENV === 'production'){
     //Express will serve up production assets
     //like our main.js file, or main.css file!
-
+    app.use(express.static('client/build'));
+    
     //Epress will serve up the index.html file
     // if it doesn't recognize the route
     const path = require ('path');
